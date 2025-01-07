2fc02ca67862723239952becaa0c6247f12ed7c3
Opposolis <wmsjshihao@gmail.com>
2025-01-07 14:27:41 +0800

    change log func to printf

--- utilities/ovs-ofctl.c
+++ utilities/ovs-ofctl.c
@@ -3044,9 +3044,7 @@ static void
 bundle_group_mod__(const char *remote, struct ofputil_group_mod *gms,
                    size_t n_gms, enum ofputil_protocol usable_protocols)
 {
-    OFPPROP_LOG(&rl, false, "bundle_group_mod__ begin");
-    VLOG_WARN_RL(&rl, "bundle_group_mod__ begin");
-    VLOG_WARN("bundle_group_mod__ begin");
+    printf("bundle_group_mod__ begin");
     enum ofputil_protocol protocol;
     enum ofp_version version;
     struct vconn *vconn;
@@ -3078,9 +3076,7 @@ static void
 ofctl_group_mod__(const char *remote, struct ofputil_group_mod *gms,
                   size_t n_gms, enum ofputil_protocol usable_protocols)
 {
-    OFPPROP_LOG(&rl, false, "ofctl_group_mod__ begin");
-    VLOG_WARN_RL(&rl, "ofctl_group_mod__ begin");
-    VLOG_WARN("ofctl_group_mod__ begin");
+    printf("ofctl_group_mod__ begin");
     enum ofputil_protocol protocol;
     struct ofputil_group_mod *gm;
     enum ofp_version version;
@@ -4201,10 +4197,7 @@ ofctl_parse_flows(struct ovs_cmdl_context *ctx)
 static void
 ofctl_parse_group(struct ovs_cmdl_context *ctx)
 {
-    OFPPROP_LOG(&rl, false, "ofctl_parse_group begin");
-    VLOG_WARN_RL(&rl, "ofctl_parse_group begin");
-    VLOG_WARN("ofctl_parse_group begin");
-    
+    printf("ofctl_parse_group begin");
     enum ofputil_protocol usable_protocols;
     struct ofputil_group_mod gm;
     char *error = parse_ofp_group_mod_str(&gm, OFPGC11_ADD, ctx->argv[1], NULL,
