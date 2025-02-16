56a7ff44e203defcc63bd8c165682caac90eabba
Opposolis <wmsjshihao@gmail.com>
2025-02-16 10:36:47 +0800

    添加pick_random_select_group_1（完全从pick_default_select_group复制）用于测试

--- ofproto/ofproto-dpif-xlate.c
+++ ofproto/ofproto-dpif-xlate.c
@@ -4995,20 +4995,20 @@ pick_dp_hash_select_group(struct xlate_ctx *ctx, struct group_dpif *group)
 static struct ofputil_bucket *
 pick_random_select_group(struct xlate_ctx *ctx, struct group_dpif *group)
 {
-     total_weight = 0;
+    uint32_t weight_total = 0;
     struct ofputil_bucket *bucket;
     LIST_FOR_EACH (bucket, list_node, &group->up.buckets) {
         if (bucket_is_alive(ctx, group, bucket, 0)) {
-            total_weight += (uint32_t)bucket->weight;
+            weight_total += (uint32_t)bucket->weight;
         }
     }
 
-    if (total_weight == 0) {
+    if (weight_total == 0) {
         return NULL; // 没有可用的桶
     }
 
     random_init(); // 可选的初始化
-    uint32_t random_value = random_uint32() % total_weight; // 使用 random_uint32()  生成 0 到 total_weight-1 的随机数
+    uint32_t random_value = random_uint32() % weight_total; // 使用 random_uint32()  生成 0 到 weight_total-1 的随机数
     uint32_t cumulative_weight = 0;
 
     LIST_FOR_EACH (bucket, list_node, &group->up.buckets) {
@@ -5024,7 +5024,14 @@ pick_random_select_group(struct xlate_ctx *ctx, struct group_dpif *group)
     return NULL;
 }
 
-
+static struct ofputil_bucket *
+pick_random_select_group_1(struct xlate_ctx *ctx, struct group_dpif *group)
+{
+    flow_mask_hash_fields(&ctx->xin->flow, ctx->wc,
+        NX_HASH_FIELDS_SYMMETRIC_L4);
+    return group_best_live_bucket(ctx, group,
+                flow_hash_symmetric_l4(&ctx->xin->flow, 0));
+}
 
 
 
@@ -5050,7 +5057,7 @@ pick_select_group(struct xlate_ctx *ctx, struct group_dpif *group)  // 可能相
         return pick_dp_hash_select_group(ctx, group);
         break;
     case SEL_METHOD_RANDOM:
-        return pick_random_select_group(ctx, group);
+        return pick_random_select_group_1(ctx, group);
         break;
     default:
         /* Parsing of groups ensures this never happens */
