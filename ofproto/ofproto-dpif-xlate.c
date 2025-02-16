ec507f9d211114ba183505a3beddab9a55075f86
Opposolis <wmsjshihao@gmail.com>
2025-02-16 16:00:24 +0800

    updated

--- ofproto/ofproto-dpif-xlate.c
+++ ofproto/ofproto-dpif-xlate.c
@@ -5024,70 +5024,79 @@ pick_random_select_group(struct xlate_ctx *ctx, struct group_dpif *group)
     redirect_stdout_to_file("/home/osboxes/Desktop/log/ovs_log_workflow_default.txt", log_message, __LINE__, __FILE__);
 
     uint32_t dp_hash = ctx->xin->flow.dp_hash;
+    if (!dp_hash) {
+        enum ovs_hash_alg hash_alg = group->hash_alg;
+        if (hash_alg > ctx->xbridge->support.max_hash_alg) {
+            /* Algorithm supported by all datapaths. */
+            hash_alg = OVS_HASH_ALG_L4;
+        }
+        ctx_trigger_recirculate_with_hash(ctx, hash_alg, group->hash_basis);
+        return NULL;
+    } else {
+        uint32_t weight_total = 0;
+        struct ofputil_bucket *bucket;
+        LIST_FOR_EACH (bucket, list_node, &group->up.buckets) {
+            if (bucket_is_alive(ctx, group, bucket, 0)) {
+                weight_total += (uint32_t)bucket->weight;
+                snprintf(log_message, sizeof(log_message), 
+                    "Bucket: %p, weight: %u, weight_total: %u", bucket, bucket->weight, weight_total);
+                // redirect_stdout_to_file("/home/osboxes/Desktop/ovs_log.txt", log_message, __LINE__, __FILE__);
+                redirect_stdout_to_file("/home/osboxes/Desktop/log/ovs_log_workflow_default.txt", log_message, __LINE__, __FILE__);
+            } else {
+                xlate_report_bucket_not_live(ctx, bucket);
+            }
+        }
 
-    uint32_t weight_total = 0;
-    struct ofputil_bucket *bucket;
-    LIST_FOR_EACH (bucket, list_node, &group->up.buckets) {
-        if (bucket_is_alive(ctx, group, bucket, 0)) {
-            weight_total += (uint32_t)bucket->weight;
-            snprintf(log_message, sizeof(log_message), 
-                "Bucket: %p, weight: %u, weight_total: %u", bucket, bucket->weight, weight_total);
+        if (weight_total == 0) {
+            snprintf(log_message, sizeof(log_message), "No available buckets, weight_total is 0");
             // redirect_stdout_to_file("/home/osboxes/Desktop/ovs_log.txt", log_message, __LINE__, __FILE__);
             redirect_stdout_to_file("/home/osboxes/Desktop/log/ovs_log_workflow_default.txt", log_message, __LINE__, __FILE__);
-        } else {
-            xlate_report_bucket_not_live(ctx, bucket);
+            return NULL; // 没有可用的桶
         }
-    }
 
-    if (weight_total == 0) {
-        snprintf(log_message, sizeof(log_message), "No available buckets, weight_total is 0");
+        // srand((unsigned int)time(NULL));
+        // random_init(); // 可选的初始化
+        uint32_t random_value = random_uint32() % weight_total; // 使用 random_uint32()  生成 0 到 weight_total-1 的随机数
+
+
+        // 记录生成的随机数
+        snprintf(log_message, sizeof(log_message), 
+            "Random value generated: %u (range: 0 to %u)", random_value, weight_total - 1);
         // redirect_stdout_to_file("/home/osboxes/Desktop/ovs_log.txt", log_message, __LINE__, __FILE__);
         redirect_stdout_to_file("/home/osboxes/Desktop/log/ovs_log_workflow_default.txt", log_message, __LINE__, __FILE__);
-        return NULL; // 没有可用的桶
-    }
 
-    // srand((unsigned int)time(NULL));
-    // random_init(); // 可选的初始化
-    uint32_t random_value = random_uint32() % weight_total; // 使用 random_uint32()  生成 0 到 weight_total-1 的随机数
+        uint32_t cumulative_weight = 0;
+        
+        struct ofputil_bucket *best_bucket = NULL;
 
+        LIST_FOR_EACH (bucket, list_node, &group->up.buckets) {
+            if (bucket_is_alive(ctx, group, bucket, 0)) {
+                cumulative_weight += bucket->weight;
 
-    // 记录生成的随机数
-    snprintf(log_message, sizeof(log_message), 
-        "Random value generated: %u (range: 0 to %u)", random_value, weight_total - 1);
-    // redirect_stdout_to_file("/home/osboxes/Desktop/ovs_log.txt", log_message, __LINE__, __FILE__);
-    redirect_stdout_to_file("/home/osboxes/Desktop/log/ovs_log_workflow_default.txt", log_message, __LINE__, __FILE__);
-
-    uint32_t cumulative_weight = 0;
-    
-    struct ofputil_bucket *best_bucket = NULL;
-
-    LIST_FOR_EACH (bucket, list_node, &group->up.buckets) {
-        if (bucket_is_alive(ctx, group, bucket, 0)) {
-            cumulative_weight += bucket->weight;
-
-            // 记录当前桶的累计权重
-            snprintf(log_message, sizeof(log_message), 
-                "Bucket: %p, cumulative_weight: %u", bucket, cumulative_weight);
-            // redirect_stdout_to_file("/home/osboxes/Desktop/ovs_log.txt", log_message, __LINE__, __FILE__);
-            redirect_stdout_to_file("/home/osboxes/Desktop/log/ovs_log_workflow_default.txt", log_message, __LINE__, __FILE__);
-
-            if (random_value < cumulative_weight) {
-                // return bucket; // 选中当前桶
-
+                // 记录当前桶的累计权重
                 snprintf(log_message, sizeof(log_message), 
-                    "Selected bucket: %p", bucket);
+                    "Bucket: %p, cumulative_weight: %u", bucket, cumulative_weight);
                 // redirect_stdout_to_file("/home/osboxes/Desktop/ovs_log.txt", log_message, __LINE__, __FILE__);
                 redirect_stdout_to_file("/home/osboxes/Desktop/log/ovs_log_workflow_default.txt", log_message, __LINE__, __FILE__);
 
-                best_bucket = bucket;
-                break;
-            }
-        } else {
-            xlate_report_bucket_not_live(ctx, bucket);
-        }
-    }
+                if (random_value < cumulative_weight) {
+                    // return bucket; // 选中当前桶
 
-    return best_bucket;
+                    snprintf(log_message, sizeof(log_message), 
+                        "Selected bucket: %p", bucket);
+                    // redirect_stdout_to_file("/home/osboxes/Desktop/ovs_log.txt", log_message, __LINE__, __FILE__);
+                    redirect_stdout_to_file("/home/osboxes/Desktop/log/ovs_log_workflow_default.txt", log_message, __LINE__, __FILE__);
+
+                    best_bucket = bucket;
+                    break;
+                }
+            } else {
+                xlate_report_bucket_not_live(ctx, bucket);
+            }
+        }
+
+        return best_bucket;
+    }
 }
 
 static struct ofputil_bucket *
